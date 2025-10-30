e70e8367bc9ce6e627d458375b8e415f96456437
ega <202210027@fit.edu.ph>
2025-10-30 15:09:53 +0800

    feat: add add/delete functionality for books

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import "./App.css";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { BookPage } from "./pages/BookPage";
 import { BookLibrary } from "./pages/BookLibrary";
 
 export default function App() {
-  const books = ["sample-book-1.txt", "sample-book-2.txt"];
-  return (
-    <Routes>
-      {/* book list page */}
-      <Route path="/" element={<BookLibrary books={books} />} />
+  const [books, setBooks] = useState([
+    "sample-book-1.txt",
+    "sample-book-2.txt",
+  ]);
+  const [bookName, setBookName] = useState("");
 
-      {/* individual book page */}
-      <Route path="/:book" element={<BookPage books={books} />} />
-    </Routes>
+  const addData = () => {
+    setBooks((prev) => [...prev, bookName]);
+  };
+
+  const handleNameChange = (event) => {
+    setBookName(event.target.value);
+  };
+
+  return (
+    <>
+      <input type="text" value={bookName} onChange={handleNameChange} />
+      <button onClick={addData}>Add</button>
+      <Routes>
+        {/* book list page */}
+        <Route
+          path="/"
+          element={<BookLibrary books={books} setBooks={setBooks} />}
+        />
+        {/* individual book page */}
+        <Route path="/:book" element={<BookPage books={books} />} />
+      </Routes>
+    </>
   );
 }
--- src/pages/BookLibrary.js
+++ src/pages/BookLibrary.js
@@ -2,16 +2,24 @@ import { Link } from "react-router-dom";
 import "../App.css";
 import { BookCard } from "../ui/BookCard";
 
-export function BookLibrary({ books }) {
+export function BookLibrary({ books, setBooks }) {
+  const handleDelete = (book) => {
+    const newBooks = books.filter((b) => b !== book);
+    setBooks(newBooks);
+  };
+
   return (
     <div className="library">
       <h2>Book List</h2>
       <div className="booklist">
         {/* loops through books to display as button cards and routing*/}
         {books.map((book) => (
-          <Link key={book} to={`/${book.replace(".txt", "")}`}>
-            <BookCard textfile={book} />
-          </Link>
+          <>
+            <Link key={book} to={`/${book.replace(".txt", "")}`}>
+              <BookCard textfile={book} />
+            </Link>
+            <button onClick={() => handleDelete(book)}>Delete</button>
+          </>
         ))}
       </div>
     </div>
